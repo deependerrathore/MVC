f62fdb9944527773ce82fb2981ccbf72de4cf93c
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-12 12:25:26 +1000

    public methods Inspector

--- classes/framework/Inspector.php
+++ classes/framework/Inspector.php
@@ -65,5 +65,59 @@ namespace Framework{
             }
             return $meta;
         }
+
+        public function getClassMeta(){
+            if(!isset($_meta["class"])){
+                $comment = $this->_getClassComment();
+                if(!empty($comment)){
+                    $_meta["class"] = $this->_parse($comment);
+                }else{
+                    $_meta["class"] = null;
+                }
+            }
+            return $_meta["class"];
+        }
+
+        public function getClassProperties(){
+            if(!isset($_properties)){
+                $properties = $this->_getClassProperties();
+                foreach ($properties as $property) {
+                    $_properties[] = $property->get();
+                }
+            }
+            return $_properties;
+        }
+        public function getClassMethods(){
+            if(!isset($_methods)){
+                $methods = $this->_getClassMethods();
+                foreach ($methods as $method) {
+                    $_methods[] = $method->getName();
+                }
+            }
+            return $_properties;
+        }
+
+        public function getPropertyMeta($property){
+            if(!isset($_meta["perperties"][$property])){
+                $comment = $this->_getPropertyComment($property);
+                if(!empty($comment)){
+                    $_meta["properties"][$property] = $this->_parse($comment);
+                }else{
+                    $_meta["properties"][$property] = null
+                }
+            }
+            return $_meta["perperties"][$property];
+        }
+        public function getMethodMeta($method){
+            if(!isset($_meta["actions"][$method])){
+                $comment = $this->_getMethodComment($method);
+                if(!empty($comment)){
+                    $_meta["methods"][$method] = $this->_parse($comment);
+                }else{
+                    $_meta["methods"][$method] = null;
+                }
+            }
+            return $_meta["methods"][$method];
+        }
     }
 }
