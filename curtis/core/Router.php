30d88d198b21480ad11386d735f3c46a5f819ea8
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-23 23:29:47 +1000

    Router class

--- curtis/core/Router.php
+++ curtis/core/Router.php
@@ -2,6 +2,26 @@
 
 class Router{
     public static function route($url){
-        dnd($url);
+        //controller
+        $controller = (isset($url[0]) && $url[0] != '')? ucfirst($url[0]) : DEFAULT_CONTROLLER ; // DEFAULT_CONTROLLER = Home in config/config.php
+        $controller_name = $controller;
+        array_shift($url);
+        
+        //action
+        $action = (isset($url[0]) && $url[0] != '')? lcfirst($url[0]) . 'Action' : 'indexAction' ; 
+        $action_name = $action;
+        array_shift($url);
+        
+        //params
+        $queryParams = $url;
+        
+        $dispatch = new $controller($controller_name,$action);
+
+        if (method_exists($controller,$action)) {
+            call_user_func_array([$dispatch,$action],$queryParams);// == $dispatch->registerAction($queryParams)
+        }else{
+            echo "{$action} does not exists in the controller {$controller}";
+        }
+        
     }
 }
--- curtis/core/bootstrap.php
+++ curtis/core/bootstrap.php
@@ -4,21 +4,26 @@
 require_once(ROOT_DIR. DS . 'config' . DS . 'config.php');
 require_once(ROOT_DIR. DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
 
-// autoload the class
-function __autoload($className){
-    if(file_exists(ROOT_DIR . DS . 'core' . DS . $className . '.php')){ //check for the files present in core folder
+// // autoload the class
+// function autoload($className){
+//     if(file_exists(ROOT_DIR . DS . 'core' . DS . $className . '.php')){ //check for the files present in core folder
 
-        require_once(ROOT_DIR . DS . 'core' . DS . $className . '.php');
+//         require_once(ROOT_DIR . DS . 'core' . DS . $className . '.php');
 
-    }elseif (file_exists(ROOT_DIR . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) { // check the files present in app/contollers folder
+//     }elseif (file_exists(ROOT_DIR . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) { // check the files present in app/contollers folder
 
-        require_once(ROOT_DIR . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+//         require_once(ROOT_DIR . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
 
-    }elseif(file_exists(ROOT_DIR . DS . 'app' . DS . 'models' . DS . $className . '.php')){ // check the files present in app/models folder
+//     }elseif(file_exists(ROOT_DIR . DS . 'app' . DS . 'models' . DS . $className . '.php')){ // check the files present in app/models folder
 
-        require_once(ROOT_DIR . DS . 'app' . DS . 'models' . DS . $className . '.php');
-    }
-}
+//         require_once(ROOT_DIR . DS . 'app' . DS . 'models' . DS . $className . '.php');
+//     }
+// }
+
+// spl_autoload_register('autoload');
+spl_autoload_extensions(".php"); // comma-separated list
+spl_autoload_register();
+
 
 // Route the request
 Router::route($url);
