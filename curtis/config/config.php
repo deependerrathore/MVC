2340b4e6820ac68e37c694e6c5b5dc49f0354cba
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-24 21:31:18 +1000

    View class part 1

--- curtis/config/config.php
+++ curtis/config/config.php
@@ -1,3 +1,8 @@
 <?php
     define('DEBUG', true);
     define('DEFAULT_CONTROLLER','Home'); // default controller if there isn't any contoller in url
+    define('DEFAULT_LAYOUT', 'default'); // if no layout has ben set in contoller use this layout
+    
+    define('PROTECT_ROOT','MVC/curtis/'); // set this to '/' for live server
+
+    define('SITE_TITLE', 'MVC_FRAMEWORK'); //this will be used if no site title will be used
---
+++ curtis/core/View.php
@@ -0,0 +1,30 @@
+<?php
+
+class View{
+    protected $_head, $_body, $_siteTitle , $_outputBuffer, $_layout = DEFAULT_LAYOUT;
+
+    public function __construct(){
+        
+    }
+
+    public function render($viewName){
+        $viewArray = explode('/',$viewName);
+        $viewString = implode(DS , $viewArray);
+        if(file_exists(ROOT . DS . 'app'. DS . 'views' . DS . $viewString. '.php')){
+            include(ROOT . DS . 'app'. DS . 'views' . DS . $viewString. '.php');
+            include(ROOT . DS . 'app'. DS . 'views' . DS . 'layouts'. DS . $this->_layout . '.php');
+        }else{
+            die("The view {$viewName} does not exist");
+        }
+    }
+
+    public function content($type){
+        if($type == 'head'){
+            return $this->_head;
+        }elseif($type == 'body'){
+            return $this->_body;
+        }
+
+        return false;
+    }
+}
