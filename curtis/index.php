fb768b9e0cea39714d6090ad8392e9e10b0610e1
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-26 23:15:35 +1000

    Refactor

--- curtis/index.php
+++ curtis/index.php
@@ -1,9 +1,36 @@
 <?php
-    session_start();
-    define('DS',DIRECTORY_SEPARATOR); //Windows = \
-    define('ROOT',__FILE__); //C:\Dev&Test\www\MVC\curtis\index.php
-    define('ROOT_DIR',__DIR__);//C:\Dev&Test\www\MVC\curtis
-    
-    $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'],'/'))  : [];
 
-    require_once(ROOT_DIR. DS . 'core' . DS . 'bootstrap.php');
+define('DS', DIRECTORY_SEPARATOR); //Windows = \
+define('ROOT', __FILE__); //C:\Dev&Test\www\MVC\curtis\index.php
+define('ROOT_DIR', __DIR__);//C:\Dev&Test\www\MVC\curtis
+    
+    
+// load configuration and helper functions
+require_once(ROOT_DIR . DS . 'config' . DS . 'config.php');
+require_once(ROOT_DIR . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+// autoload the class
+function autoload($className)
+{
+    if (file_exists(ROOT_DIR . DS . 'core' . DS . $className . '.php')) { //check for the files present in core folder
+
+        require_once(ROOT_DIR . DS . 'core' . DS . $className . '.php');
+
+    } elseif (file_exists(ROOT_DIR . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) { // check the files present in app/contollers folder
+
+        require_once(ROOT_DIR . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+
+    } elseif (file_exists(ROOT_DIR . DS . 'app' . DS . 'models' . DS . $className . '.php')) { // check the files present in app/models folder
+
+        require_once(ROOT_DIR . DS . 'app' . DS . 'models' . DS . $className . '.php');
+    }
+}
+
+
+spl_autoload_register('autoload');
+session_start();
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+    
+// Route the request
+Router::route($url);
