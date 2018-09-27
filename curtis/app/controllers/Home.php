aafe4fbe50dd1dfeb875d9a75cfbbfdfe280d0b7
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-27 19:51:02 +1000

    DB wrapper part 2 - query function

--- curtis/app/controllers/Home.php
+++ curtis/app/controllers/Home.php
@@ -7,6 +7,11 @@ class Home extends Controller{
 
     public function indexAction(){
         //die('Welcome to the home controller this is a indexAction'); 
+
+        $db = DB::getInstance();
+        $sql = 'SELECT * from contacts';
+        $contacts = $db->query($sql);
+        dnd($contacts);
         $this->view->render('home/index');
     }
 }
--- curtis/core/DB.php
+++ curtis/core/DB.php
@@ -2,7 +2,7 @@
 
 class DB{
     private static $_instance = null;
-    private $_pdo, $query , $_error = false , $_result , $count = 0,$_lastInsertID = null;
+    private $_pdo, $_query , $_error = false , $_result , $_count = 0,$_lastInsertID = null;
 
     private function __construct(){
         try{
@@ -17,5 +17,26 @@ class DB{
             self::$_instance = new DB();
         }
         return self::$_instance;
+    }
+
+    public function query($sql, $params = []){
+        $this->_error = false;
+        if($this->_query = $this->_pdo->prepare($sql)){
+            $x = 1;
+            if(count($params)){
+                foreach($params as $param){
+                    $this->_query->bindValue($x,$param);
+                    $x++;
+                }
+            }
+        }
+        if($this->_query->execute()){
+            $this->_result = $this->_query->fetchAll(PDO::FETCH_OBJ);
+            $this->_count = $this->_query->rowCount();
+            $this->_lastInsertID = $this->_pdo->lastInsertId();
+        }else{
+            $this->_error = true;
+        }
+        return $this;
     }
 }
