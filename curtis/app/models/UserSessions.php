57e3bdc4488c48e32823a1e6dfd0b685b7043e80
Deepender Rathore <deepenrathore28@gmail.com>
2018-10-14 17:41:45 +1100

    User Session Modal refactor

--- curtis/app/models/UserSessions.php
+++ curtis/app/models/UserSessions.php
@@ -5,4 +5,21 @@ class UserSessions extends Model{
         $table = 'user_sessions';
         parent::__construct($table);
     }
+
+    public static function getFromCookie(){
+
+        $userSession = new self();
+        dnd($userSession->get_columns());
+        if(COOKIE::exists(REMEMBER_ME_COOKIE_NAME)){
+            $userSession = $userSession->findFirst([
+                'conditions' => "user_agent = ? AND session = ?",
+                'bind' => [Session::uagent_no_version(),COOKIE::get(REMEMBER_ME_COOKIE_NAME)]
+            ]);
+        }
+
+        if(!$userSession) return false;
+        return $userSession;
+        
+    }
+
 }
